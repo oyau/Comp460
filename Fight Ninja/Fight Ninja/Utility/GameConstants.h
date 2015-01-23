b55d5fed9351bbbef0391e4cf190d5f78283254e
oyau <olyver.yau@gmail.com>
2015-01-22 19:39:46 -0600

    First commit

---
+++ Fight Ninja/Fight Ninja/Utility/GameConstants.h
@@ -0,0 +1,25 @@
+//
+//  GameConstants.h
+//  Cocos2DSimpleGame
+//
+//  Created by Rajeev on 25/01/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+
+#define APPWARP_APP_KEY     @"ec4c34c1aa663cdb8f3786c2d866e9d57c2d297b1e054089d7a707edb00ba02d"
+#define APPWARP_SECRET_KEY  @"fc4291badd7feef7194ddb0f374201f73615ca0ebda20358b6eddf1ee8d79d53"
+
+#define GAME_NAME           @"NinjaFight"
+#define ROOM_NAME           @"FightNinjaTest"
+#define ROOM_OWNER          @"Olyver"
+#define MAX_PLAYER          2
+#define GAME_OVER_TIME      300.0f //In Seconds
+
+#define USER_NAME           @"userName"
+#define PLAYER_POSITION     @"playerPositon"
+#define PROJECTILE_DESTINATION    @"projectileDest"
+#define MOVEMENT_DURATION    @"realMoveDuration"
+
+
+#define MAX_NUMBER_OF_RECORDS_DISPLAYED_IN_LB   20
